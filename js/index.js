94773b465c3bd2bab4e8c59c86b7503714c45aa7
ChoPC <whwotjd9090@gmail.com>
2023-08-27 02:32:23 +0900

    Change to scroll version

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-new PureFullPage();
-
-// const img_modal = document.getElementById('gallery-modal');
-// const gpzs = document.getElementsByClassName('gallery-photo-zone');
-// const modal_img = document.getElementById("gallery-modal-img");
-
-// for(var i = 0; i < gpzs.length; i++) {
-//   gpzs[i].getElementsByTagName('img')[0].addEventListener('click', (e) => {
-//     img_modal.style.display = 'block';
-
-//     var pos = e.target.src.lastIndexOf('/');
-//     modal_img.src = e.target.src.slice(0, pos) + "/org" + e.target.src.slice(pos);
-//   });
-// }
-
-// img_modal.addEventListener('click', () => {
-//   img_modal.style.display = 'none';
-// });
-
 const gpzs = document.querySelectorAll('.gallery-photo-zone');
 const imageView = document.querySelector('.image-view');
 const nextBtn = document.getElementById('next-btn');
@@ -29,7 +10,6 @@ imageView.addEventListener('click', function(){
     this.style.display = "none";
     imageBox.style.display = "none";
 })
-
 
 gpzs.forEach(function(btn, index){
     btn.addEventListener('click', function(){
